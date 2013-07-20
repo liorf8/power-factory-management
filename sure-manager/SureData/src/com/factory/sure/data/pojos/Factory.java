a1586a42e2072bbabbd4d5ac3dd7528a0978f39f
minhluan1590 <minhluan1590@gmail.com>
2013-07-20 10:11:11 +0000

    Add test feature without comport. Can also store to database.

--- sure-manager/SureData/src/com/factory/sure/data/pojos/Factory.java
+++ sure-manager/SureData/src/com/factory/sure/data/pojos/Factory.java
@@ -1,24 +1,18 @@
 package com.factory.sure.data.pojos;
 
-import java.util.ArrayList;
-import java.util.Set;
+import java.util.List;
 
 public class Factory {
-    private ArrayList<Generator> m_pGenerators;
+    private List<Generator> m_pGenerators;
 
-    public Factory(Set<Byte> modbusAddressSet) {
-        // Asume that modbusAddress != null
-        this.m_pGenerators = new ArrayList<Generator>(modbusAddressSet.size());
-        for (Byte b : modbusAddressSet) {
-            Generator gen = new Generator(b.byteValue());
-            this.m_pGenerators.add(gen);
-        }
+    public Factory(List<Generator> generatorList) {
+        this.m_pGenerators = generatorList;
     }
 
     /**
      * @return the m_pGenerators
      */
-    public ArrayList<Generator> getGenerators() {
+    public List<Generator> getGenerators() {
         return m_pGenerators;
     }
 }
